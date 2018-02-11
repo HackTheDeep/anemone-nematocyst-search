22a632dc443ffcbda914858b026152c340d819ff
aphuong <aphuong85@gmail.com>
2018-02-11 10:03:41 -0500

    update js

--- app/assets/javascripts/search-js.js
+++ app/assets/javascripts/search-js.js
@@ -1,23 +1,28 @@
 $(function() {
   $('.search').click(function(e) {
     e.preventDefault();
+    var sliders  = $('.sliderValue');
+    var values = []
+    $.each(sliders, function(sliderId, slider) {
+      values.push(slider.value.split(' - '));
+    });
 
     var featureParams = {
       1: {
-        min: 2,
-        max: 100
+        min: values[0][0],
+        max: values[0][1]
       },
       2: {
-        min: 1,
-        max: 100
+        min: values[1][0],
+        max: values[1][1]
       },
       3: {
-        min: 15,
-        max: 350
+        min: values[2][0],
+        max: values[2][1]
       },
       4: {
-        min: 6,
-        max: 120
+        min: values[3][0],
+        max: values[3][1]
       }
     }
 
@@ -26,19 +31,24 @@ $(function() {
       url: '/search?' + $.param(featureParams)
     }).done(function(response) {
       var images = [];
-      console.log("Searched and got:", response.images)
       if (!$.isEmptyObject(response.images)) {
         $.each(response.images, function(imageId, imageData) {
           var url = imageData.url;
+          var taxa = imageData.taxa;
+          var tissue = imageData.tissue;
+          var specimen = imageData.specimen;
           var meas = imageData.measurements;
 
           var html = "<div class='col-sm-4 image-unit'> \
-          <img src=" + url + "></div> \
-          <div>Metrics:" + JSON.stringify(meas) + "</div>"
+          <img src=" + url + ">\
+          <div class='metrics'>Metrics:" + JSON.stringify(meas) + "</div> \
+          </div>"
           images.push(html);
         });
+        $('#results').html(images);
+      } else {
+        $('#results').html("<h2>No Match Found</h2>");
       }
-      $('#results').html(images);
     });
   });
 });
--- app/assets/javascripts/sliders.js
+++ app/assets/javascripts/sliders.js
@@ -1,36 +1,23 @@
-/* Slider contructor with  min and max sliders  */
-
-// $(document).ready(function() {
-//   $("#slider").slider({
-//     min: 0,
-//     max: 100,
-//     step: 1,
-//     values: [10, 90],
-//     slide: function(event, ui) {
-//       for (var i = 0; i < ui.values.length; ++i) {
-//         $("input.sliderValue[data-index=" + i + "]").val(ui.values[i]);
-//       }
-//     }
-//   });
-
-//   $("input.sliderValue").change(function() {
-//     var $this = $(this);
-//     $("#slider").slider("values", $this.data("index"), $this.val());
-//   });
-// });
-
-
 $( function() {
   $( ".slider-range" ).slider({
     range: true,
     min: 0,
     max: 200,
-    values: [ 30, 100 ],
+    values: [ 1, 200 ],
     slide: function( event, ui ) {
-      console.log(event);
-      // $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+      console.log(event.target.id);
+      var slider = event.target.id;
+      for (var i = 0; i < ui.values.length; ++i) {
+        $("input.sliderValue[data-index=" + slider + "]").val(ui.values[0] + " - " + ui.values[1]);
+      }
     }
   });
   // $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-    // " - $" + $( "#slider-range" ).slider( "values", 1 ) );
-} );
+  //   " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+});
+
+
+
+
+
+
